e3d4197957bba2c628fa10c1b019ad5827936cc4
NarmadhaAD <92127875+NarmadhaAD@users.noreply.github.com>
2021-11-27 20:01:57 +0530

    Add files via upload

---
+++ thisKEY.java
@@ -0,0 +1,18 @@
+class calc {
+    int n = 40, m = 50;
+
+    public calc(int n, int m) {
+        // this.n = n;
+        // this.m = m;
+    }
+
+}
+
+public class thisKEY {
+
+    public static void main(String args[]) {
+        calc obj = new calc(4, 5);
+        System.out.println(obj.n);
+        System.out.println(obj.m);
+    }
+}
